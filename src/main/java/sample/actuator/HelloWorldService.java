aa0cfe28df496ceea3080ee8ffcd913298bc4981
Ed Earley <ed@otus.com>
2019-03-08 15:09:36 -0600

    test the pipeline again

--- src/main/java/sample/actuator/HelloWorldService.java
+++ src/main/java/sample/actuator/HelloWorldService.java
@@ -24,7 +24,7 @@ public class HelloWorldService {
 	
 
 	public String getHelloMessage() {
-		return "Spring boot says hello Otus from a Docker container";
+		return "Ed says hello Otus from a Docker container";
 	}
 
 }
